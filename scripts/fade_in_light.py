03d96c677d791d0f97aaa62067fba246839c29e8
va1entin <valh@mailbox.org>
2021-01-13 19:18:03 +0100

    use json function from requests, fix custom host handling

--- scripts/fade_in_light.py
+++ scripts/fade_in_light.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 import argparse
-import json
 import requests
 
 
 def setup_parser():
     parser = argparse.ArgumentParser()
-
     parser.add_argument('-g', '--group', help='deCONZ group name', required=True)
     parser.add_argument('-b', '--brightness', help='Brightness level', type=int, required=True)
     parser.add_argument('-t', '--time', help="Transition time in minutes", type=int, required=True)
     parser.add_argument('-p', '--port', help="deCONZ port", type=int, required=True)
     parser.add_argument('-k', '--api-key', help="API key", required=True)
     parser.add_argument('-H', '--host', help="deCONZ host", default="localhost")
-
     args = parser.parse_args()
-
     return args
 
 
 def main(args):
-    response = requests.get(f'http://localhost:{args.port}/api/{args.api_key}/groups')
-    groups = json.loads(response.text)
+    response = requests.get(f'http://{args.host}:{args.port}/api/{args.api_key}/groups')
+    groups = response.json()
 
     for group_id, group in groups.items():
         if group['name'] == args.group:
